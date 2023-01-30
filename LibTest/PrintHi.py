4b75b18e3f9c9163078ee9bb4bc16bcbfabceb1b
Andrij Demianczuk <andrij.demianczuk@databricks.com>
2023-01-30 13:34:26 -0700

    updated greeting

--- LibTest/PrintHi.py
+++ LibTest/PrintHi.py
@@ -2,7 +2,7 @@ class Hello:
 
     def __init__(self):
         pass
-        self.greeting = "Hello Jason!"
+        self.greeting = "Hello Jason and Sergii!"
 
     def sayHi(self) -> str:
         return self.greeting
