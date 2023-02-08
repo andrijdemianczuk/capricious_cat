22d0426da4777aa6f4c3199df07c5b48d4baaf9d
andrijdemianczuk <andrijdemianczuk>
2023-02-08 17:53:55 +0000

    first function commit

--- Entities/dew.py
+++ Entities/dew.py
@@ -1,3 +1,18 @@
-import ABC
+from abc import abstractmethod, abstractproperty, abstractclassmethod, ABC
+import datetime as dt #Might want to be specific around the functions we need
 
-class dew():
+class Dew(ABC):
+  
+  def __init__(self):
+    pass
+
+  @abstractmethod
+  def load(self):
+    pass
+  
+  def add_bronze_metadata_cols(self, spark, df):
+    df = (df.withColumn("source_file_name", input_file_name())
+        .withColumn("bronze_loaded_timestamp_utc", current_timestamp())
+        .withColumn("date_key", lit(f"{dt.datetime.now():%Y%m%d}")))
+    print("transform_raw functions loaded")
+    return (df)
