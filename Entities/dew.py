d0eb6c1227e873dbb86bb4b3ecf9b57978cd6686
andrijdemianczuk <andrijdemianczuk>
2023-02-09 20:27:34 +0000

    added class framework to dew functions

--- Entities/dew.py
+++ Entities/dew.py
@@ -1,18 +1,44 @@
-from abc import abstractmethod, abstractproperty, abstractclassmethod, ABC
+from abc import abstractmethod, abstractproperty, ABC
 import datetime as dt #Might want to be specific around the functions we need
 
-class Dew(ABC):
-  
+class DewStream(ABC):
+
   def __init__(self):
-    pass
+    self.global_flag = 0
+  
+  @abstractmethod
+  def read_stream_raw_autoloader(self):
+    return ("reading stream")
 
   @abstractmethod
-  def load(self):
+  def  write_stream_bronze_delta_trigger_once(self):
+    return ("writing stream")
+
+class DewFn(DewStream):
+
+  def __init__(self):
     pass
   
+  def read_stream_raw_autoloader(self):
+    #Directly composed from the base class - since we don't need to override for any specific reason we can just use whatever's in the base class.
+    return super().read_stream_raw_autoloader()
+
+  def  write_stream_bronze_delta_trigger_once(self):
+    pass
+
+  def encrypt_cols(self):
+    return ("Encrypting cols")
+
   def add_bronze_metadata_cols(self, spark, df):
-    df = (df.withColumn("source_file_name", input_file_name())
-        .withColumn("bronze_loaded_timestamp_utc", current_timestamp())
-        .withColumn("date_key", lit(f"{dt.datetime.now():%Y%m%d}")))
-    print("transform_raw functions loaded")
-    return (df)
+    return ("Adding metadata cols")
+    # df = (df.withColumn("source_file_name", input_file_name())
+    #     .withColumn("bronze_loaded_timestamp_utc", current_timestamp())
+    #     .withColumn("date_key", lit(f"{dt.datetime.now():%Y%m%d}")))
+    # print("transform_raw functions loaded")
+    # return (df)
+
+#sample calls for testing
+k = DewFn()
+print(k.read_stream_raw_autoloader())
+print(k.add_bronze_metadata_cols("blah", "blah"))
+print(k.write_stream_bronze_delta_trigger_once())
