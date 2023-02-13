c26e6c7877d1882d4e51fd2583c87bbe4a969f9a
andrijdemianczuk <andrijdemianczuk>
2023-02-13 02:40:05 +0000

    added final function
    worked out abstract base methods vs. derived methods

--- Entities/dew_base.py
+++ Entities/dew_base.py
@@ -6,9 +6,32 @@ class DewStream(ABC):
     self.global_flag = 0
   
   @abstractmethod
-  def read_stream_raw_autoloader(self):
-    return ("reading stream")
+  def read_stream_raw_autoloader(self, 
+    spark="{spark_context}", 
+    autoloader_config:dict={}, 
+    rawPath:str=""):
+    
+    return (spark
+      .readStream
+      .format("cloudFiles")
+      .options(**autoloader_config)
+      .load(rawPath))
+
 
   @abstractmethod
-  def  write_stream_bronze_delta_trigger_once(self):
-    return ("writing stream")
+  def  write_stream_bronze_delta_trigger_once(self, 
+    spark="{spark_context}", 
+    df="{dataframe_struct}", 
+    key:str="date_key",
+    bronzePath:str="/", 
+    bronzeCheckpoint:str="/",
+    mergeSchema:str="true",
+    mode:str="append"):
+    
+    return (df.writeStream.partitionby(key)
+            .format("delta")
+            .option("checkpointlocation", bronzeCheckpoint)
+            .option("mergeSchema", mergeSchema)
+            .outputmMode(mode)
+            .trigger("once")
+            .start(bronzePath))
